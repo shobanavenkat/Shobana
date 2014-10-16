a4628af7a75270f0e3177121a5b6d86638f063a7
Christopher Brown <cmbrown1598@gmail.com>
2014-10-16 15:31:59 -0700

    Second commit.

---
+++ TestShobanaLibrary/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestShobanaLibrary
+{
+    public class MathWhiz
+    {
+        public int Add(int x, int y)
+
+        {
+
+            return x + y;
+
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestShobanaLibrary;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestTheAddMethod()
+        {
+            const int value1 = 4;
+            const int value2 = 6;
+            const int result = 10;
+
+            var whiz = new MathWhiz();
+            var classResult = whiz.Add(value1, value2);
+
+            Assert.AreEqual(result, classResult);
+        }
+
+        [TestMethod]
+        public void TestTheAddMethodAgain()
+        {
+            const int value1 = 14;
+            const int value2 = 16;
+            const int result = 30;
+
+            var whiz = new MathWhiz();
+            var classResult = whiz.Add(value1, value2);
+
+            Assert.AreEqual(result, classResult);
+        }
+    }
+}
